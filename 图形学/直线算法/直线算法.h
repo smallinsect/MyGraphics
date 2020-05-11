d4e804cdaf768b9c839fef294602f4c0b122dc42
xuqiulin <1161851688@qq.com>
2020-05-11 22:58:45 +0800

    直线算法

---
+++ 图形学/直线算法/直线算法.h
@@ -0,0 +1,33 @@
+﻿
+// 直线算法.h: 直线算法 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// C直线算法App:
+// 有关此类的实现，请参阅 直线算法.cpp
+//
+
+class C直线算法App : public CWinApp
+{
+public:
+	C直线算法App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C直线算法App theApp;
---
+++ 图形学/直线算法/直线算法View.h
@@ -0,0 +1,49 @@
+﻿
+// 直线算法View.h: C直线算法View 类的接口
+//
+
+#pragma once
+
+
+class C直线算法View : public CView
+{
+protected: // 仅从序列化创建
+	C直线算法View() noexcept;
+	DECLARE_DYNCREATE(C直线算法View)
+
+// 特性
+public:
+	C直线算法Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~C直线算法View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 直线算法View.cpp 中的调试版本
+inline C直线算法Doc* C直线算法View::GetDocument() const
+   { return reinterpret_cast<C直线算法Doc*>(m_pDocument); }
+#endif
+
